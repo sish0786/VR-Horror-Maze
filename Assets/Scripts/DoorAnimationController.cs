3514d55ba3278e0a6e11139905be10dfd72585c5
Sheharoze <sheharozuddin@gmail.com>
2023-11-05 03:15:07 -0500

    required files

---
+++ Assets/Scripts/DoorAnimationController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class DoorAnimationController : MonoBehaviour
+{
+    private Animator doorAnimator;   // The Animator on the DoorHinge
+    private AudioSource audioSource; // The AudioSource on the DoorHinge
+    public AudioClip doorOpenClip;  // Drag your doorOpen sound clip here in the inspector
+    public AudioClip doorCloseClip; // Drag your doorClose sound clip here in the inspector
+    private bool isOpen = false;    // Track door state
+
+    void Start(){
+        doorAnimator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    // This function will be called to toggle the door animation
+    public void ToggleDoorAnimation()
+    {
+        isOpen = !isOpen;
+
+        doorAnimator.SetTrigger("toggleDoor");
+
+        // Play the respective sound
+        if (isOpen)
+        {
+            audioSource.clip = doorOpenClip;
+        }
+        else
+        {
+            audioSource.clip = doorCloseClip;
+        }
+        audioSource.Play();
+    }
+}
